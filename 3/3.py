e73619a729916e45aeff274697f91a0a610890a0
Alireza Aghamohammadi <al.aghamohammadi@gmail.com>
2022-05-27 06:51:28 +0430

    add a 3rd session code

---
+++ 3/3.py
@@ -0,0 +1,15 @@
+from pathlib import Path
+
+import pandas as pd
+
+dpath = Path("../data")
+
+df = pd.read_csv(dpath / "all-states-history.csv")
+
+print(df.head())
+print(df.tail())
+
+print(df.dtypes)
+print(df.dtypes.value_counts())
+
+print(df.info())
